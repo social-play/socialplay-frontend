173ad79669ea72411d2b3bc3785f70ad0e7b95ef
Your Name <you@example.com>
2023-03-21 14:50:22 +0100

    switch button works

--- src/pages/PageRegister.tsx
+++ src/pages/PageRegister.tsx
@@ -60,7 +60,7 @@ export const PageRegister = (props: IPageRegisterProps) => {
   };
 
   return (
-    <>
+    <div className="pageRegister">
       {registrationSuccessful ? (
         <div className="registrationSuccessfulArea">
           <p>Thank you for registering!</p>
@@ -132,8 +132,7 @@ export const PageRegister = (props: IPageRegisterProps) => {
                       setFormValues({ ...formValues, isOver16: true })
                     }
                   />
-                  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                  <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+                  <div className="w-11 h-6 bg-red-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-cyan-600"></div>
                 </label>
               </div>
               <div className="buttonRow">
@@ -155,6 +154,6 @@ export const PageRegister = (props: IPageRegisterProps) => {
           )}
         </>
       )}
-    </>
+    </div>
   );
 };
