cc17a417823aefbc55ed186d878e046bee843fd8
Your Name <you@example.com>
2023-03-23 14:26:24 +0100

    editFormButton in

--- src/pages/PageProfile.tsx
+++ src/pages/PageProfile.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IUser } from "../interfaces";
 interface IPageMembersProps {
   currentUser: IUser;
@@ -5,6 +6,16 @@ interface IPageMembersProps {
 
 export const PageProfile = (props: IPageMembersProps) => {
   const { currentUser } = props;
+  const [showForm, setShowForm] = useState<boolean>(false);
+
+  const handleEditBook = () => {
+    setShowForm(true);
+  };
+
+  const handleCancelEditBook = () => {
+    setShowForm(false);
+  };
+
   return (
     <>
       <div className="userFullName">
@@ -12,6 +23,18 @@ export const PageProfile = (props: IPageMembersProps) => {
           {currentUser.firstName} {currentUser.lastName}
         </span>
       </div>
+      <button type="button" onClick={handleEditBook}>
+        Edit
+      </button>
+      {showForm && (
+        <form>
+          <input type="text" value={currentUser.firstName} />
+          <input type="text" value={currentUser.lastName} />
+          <button type="button" onClick={handleCancelEditBook}>
+            Cancel
+          </button>
+        </form>
+      )}
       <p>welcome to the Profile page</p>
     </>
   );
