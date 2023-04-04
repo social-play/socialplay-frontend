0e72660256f9b8c6ef4218537bb25d4ae9c7a6d6
Your Name <you@example.com>
2023-04-04 13:13:25 +0200

    pagewelcome finished

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -41,7 +41,7 @@ export const PageWelcome = (props: IPageMembersProps) => {
         <title>{appTitle} - Welcome</title>
       </Helmet>
 
-      <h2>{gamesPosts.length} TEAMS IN SEARCH OF PLAYERS</h2>
+      <h2>{gamesPosts.length} REQUESTS IN SEARCH FOR PLAYERS OR TEAMS</h2>
       {!isAdding ? (
         <button
           className="text-xl text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
